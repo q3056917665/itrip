004b26ceff510f1f547ccee1c132845b3ec2cfa0
q3056917665 <3056917665@qq.com>
2019-04-11 17:14:56 +0800

    q3056917665

--- src/main/java/com/example/itrip/ItripApplication.java
+++ src/main/java/com/example/itrip/ItripApplication.java
@@ -1,9 +1,13 @@
 package com.example.itrip;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example")
+@MapperScan("com.example.dao")
 public class ItripApplication {
 
 	public static void main(String[] args) {
