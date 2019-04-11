dade4e9c8abc2613f7edd990706819a3675241e0
q3056917665 <3056917665@qq.com>
2019-04-11 14:12:05 +0800

    q3056917665

---
+++ src/main/java/com/example/itrip/ItripApplication.java
@@ -0,0 +1,13 @@
+package com.example.itrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItripApplication.class, args);
+	}
+
+}
